dca474cf43a791489ae9b99e7ce49a0b2b7ec437
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-07-29 19:15:37 -0400

    Create ecc.py

--- ecc.py
+++ ecc.py
@@ -1,11 +1,11 @@
 import curves
+from sha512 import *
+from hashlib import sha1
 
 # same p,a,b parameters have to be used by both parties for 
 # creating a shared key, the shared key will be used for symmetric encryption and elliptic cryptography digital signatures
-def gen_shared_key(pri_key,pub_k, p, a):
+def gen_shared_key(pri_k,pub_k, p, a):
     return curves.montgomery_ladder(pub_k, pri_k, p, a)[0]
-
-# For hashing, sha512 is used. use hashlib for more hashing algorithms
 
 # Hash-based Message Authentication Code
 class Hmac:
@@ -15,58 +15,81 @@ class Hmac:
 class Cmac:
     pass
 
+# sources (For finding resources, not representation)
+# ANSI X9.62
+# https://www.cs.miami.edu/home/burt/learning/Csc609.142/ecdsa-cert.pdf
+#  only for odd prime field size p. For field size q = 2^m, use integer conversion specified in ANSI X9.62
 class Ecdsa:
     def __init__(self, p, G, n, a):
         self.p = p
-        self.a = a
-        self.n = n
         self.G = G
+        self.n = n
+        self.a = a
     
     # generate A's signature
-    def gen_signature(message: str, pri_key: int, key: int = None):
+    def gen_signature(self, message: str, pri_key: int, key: int = None):
         if key == 0:
             raise Exception("key x is zero")
         
-        m_hash = int(Sha512(message),16)
+        m_hash = int(str(sha1(message.encode()).hexdigest()),16)
+        self.m_hash = m_hash
         y = 0
+
+        # make sure y is not zero
         while y == 0:
             if key == None:
                 self.key = 0
                 
                 # make sure the generated key is not zero
-                while self.key == 0:
-                    new_key = curves.gen_key(self.n)
-                    self.key = gen_shared_key(new_key,self.G,
-                                              self.p,self.a)
+                # while self.key == 0:
+                new_key = 500#curves.gen_key(self.n)
+                    # self.key = gen_shared_key(new_key,self.G,
+                    #                           self.n,self.a)
+                ks = new_key+self.n
+                kt = ks+self.n
+                if len(bin(ks)[2:]) == len(bin(self.n)[2:]):
+                    p1 = curves.montgomery_ladder(self.G,kt,self.p,self.a)
+                else:
+                    p1 = curves.montgomery_ladder(self.G,ks,self.p,self.a)
+                self.key = p1[0] % self.n
             else:
+                # use key provided as function parameter
                 self.key = key
-                
+            
             # calculate modular inverse of key
-            inv_key = pow(self.key,-1,self.n) % self.n
+            inv_key = pow(self.key,-1,self.n)
             
             # calculate y coordinate of signature
-            y = inv_key*(m_hash + pri_key*self.key) % self.n
+            y = ((m_hash + pri_key * self.key) * (pow(new_key, -1,self.n))) % self.n
+            #(inv_key*(m_hash + pri_key*self.key)) % self.n
+                        
+            # if input key returns y = 0, exit with custom error message to
+            # avoid endless loop
+            if key != None and y == 0:
+                raise Exception("inputted key returns y = 0")
+            
+            
             self.signature = (self.key,y)
         return self.signature
     
     # let B verify A's signature
-    def verify_signature(signature: tuple, message: str, a_pub_key: tuple):
+    def verify_signature(self, signature: tuple, m_hash: int, a_pub_key: tuple):
         # verify that the signature point is generated correctly
         for i in range(2):
             if signature[i] == 0 or signature[i] > self.n:
                 raise Exception("signature is zero or bigger than n")
-        m_hash = int(Sha512(message),16)
-        inv_y = pow(sigmature[1],-1,self.n) % self.n
+        
+        inv_y = pow(signature[1],-1,self.n)
         u1 = (m_hash*inv_y) % self.n
         u2 = (signature[0]*inv_y) % self.n
-        mul_g_u1 = curves.montgomery_ladder(self.G,u1,self.p,self.a)
-        mul_a_pub_key_u2 = curves.montgomery_ladder(a_pub_key,u2,self.p,self.a)
-        pointX = curves.point_add(mul_g_u1[0],mul_g_u1[1],
-                                  mul_a_pub_key_u2[0],mul_a_pub_key_u2[1],
+        g_u1 = curves.montgomery_ladder(self.G,u1,self.p,self.a)
+        a_pub_key_u2 = curves.montgomery_ladder(a_pub_key,u2,self.p,self.a)
+        pointX = curves.point_add(g_u1[0],g_u1[1],
+                                  a_pub_key_u2[0],a_pub_key_u2[1],
                                   self.p,self.a)
-        
+        print("signature sign: ",pointX)
         if pointX == (0,1):
-            print("pointX points to infinity")
+            print("generated verification signature is point at infinity")
             return float("inf")
         
         if pointX[0] == signature[0]:
@@ -79,5 +102,5 @@ class Ecies:
 # Elliptic Cryptography Diffie Hellman - Elliptic Cryptography Digital 
 # Signature Algorithm - 256-bit Advanced Encryption Standard - 
 # 512-bit Secure Hashing Algorithm
-class ecdh_ecdsa_aes256_sha512:
+class ecdhe_ecdsa_aes256_sha512:
     pass
