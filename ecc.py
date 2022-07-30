6708835bccafd2674c6431420f5eba4871308e38
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-07-30 19:16:54 -0400

    Create ecc.py

--- ecc.py
+++ ecc.py
@@ -1,55 +1,138 @@
+import curves
 from sha512 import *
-# from aes import *
-from curves import *
-from ecc import *
+from hashlib import sha1,sha256,sha384 # optional
+from aes import *
 
-# TODO: test ecc.field_e_to_int function using non-prime fields
-# TODO: have hash function where it can be anything and not just sha512
-# TODO: make ECDSA implementation compatible with non-prime galois field
+# same p,a,b parameters have to be used by both parties for 
+# creating a shared key, the shared key will be used for symmetric encryption and elliptic cryptography digital signatures
+def gen_shared_key(pri_k,pub_k, p, a):
+    return curves.montgomery_ladder(pub_k, pri_k, p, a)[0]
 
-curve = Secp521r1()
-alice_secp521r1 = Secp521r1()
-bob_secp521r1 = Secp521r1()
-weierstrass = Weierstrass(bob_secp521r1.p,bob_secp521r1.a,
-                          bob_secp521r1.b)
-alice = Curve(curve)
-bob = Curve(curve)
+# field element to integer, used for non-prime fields
+# m = None is when prime field, not tested. Test with sect571k1
+def field_e_to_int(a, size, m=None):
+    if m == None:
+        return a
+    x = 0
+    bits = bin(a)[2:].zfill(m)
+    for i in range(m):
+        x = (x + int(bits[i])*2**i) % size
+    return x
 
-# generate private keys of both Alice and Bob
-alice.get_privkey(5848086670634336295179241537947744107056560678941168314791178081878633088455617469991013034321652554105110886298371233598544051928031241656197349434021052)
-bob.get_privkey(65215690354676615037081482303758947066186667230878659726538709580154848544607330564152279281874441250878379865736772667630609470695976953483213929929888987)
+# Hash-based Message Authentication Code
+def hmac(key: bytearray, msg: str,block_s: int = 64, machashf=Sha512):
+    if len(key) == block_s:
+        new_key = bytearray(key)
+    elif len(key) > block_s:
+        new_key = machashf(key).digest()
 
-# generate public keys of both Alice and Bob
-alice.get_pubkey()
-bob.get_pubkey()
+        # pad
+        for i in range(block_s-len(new_key)):
+            new_key += b'\x00'
+    else:
+        # pad
+        new_key = key
+        for i in range(len(key),block_s):
+            new_key += b'\x00'
+    
+    # calculate opad and ipad
+    ipadkey,opadkey = bytearray(),bytearray()
+    for i in range(block_s):
+        ipadkey.append(new_key[i] ^ 0x36)
+        opadkey.append(new_key[i] ^ 0x5c)
+    return machashf(opadkey + machashf((ipadkey + 
+                     msg.encode())).digest()).hexdigest()
 
-print("Alice\'s private key:\t", alice.pri_k)
-print("Bobs\'s private key:\t", bob.pri_k)
+# Cipher-based Message Authentication Code
+class Cmac:
+    def __init__(cipher=Aes256):
+        pass
 
-print("Alice\'s public key:\t", alice.pub_k)
-print("Bobs\'s public key:\t", bob.pub_k)
+#  only for odd prime field size p. For field size q = 2^m, use integer 
+#  conversion specified in ANSI X9.62
+class Ecdsa:
+    def __init__(self, curve=curves.Secp521r1):
+        if hasattr(curve,"p"):
+            self.p = curve.p
+        else:
+            self.m = curve.m
+            self.size = curve.size
+        self.G = curve.G
+        self.n = curve.n
+        self.a = curve.a
+    
+    # generate A's signature
+    def gen_signature(self, message: str, pri_key: int, key: int = None,hashf=Sha512):
+        if key == 0:
+            raise Exception("key x is zero")
+        
+        m_hash = int(str(hashf(message).hexdigest()),16)
+        self.m_hash = m_hash
+        y = 0
 
-# get compressed points
-alice_pubk_comp = '0' + str(2 + alice.pub_k[1] % 2) + \
-                  str(hex(alice.pub_k[0])[2:])
-bob_pubk_comp = '0' + str(2 + bob.pub_k[1] % 2) + \
-                str(hex(bob.pub_k[0])[2:])
+        # make sure y is not zero
+        while y == 0:
+            if key == None:
+                self.key = 0
+                
+                # make sure the generated key is not zero
+                while self.key == 0:
+                    new_key = curves.gen_key(self.n)
+                    kn = new_key+self.n
+                    knn = kn+self.n
+                    if len(bin(kn)[2:]) == len(bin(self.n)[2:]):
+                        mul = curves.montgomery_ladder(self.G,knn,self.p,self.a)
+                    else:
+                        mul = curves.montgomery_ladder(self.G,kn,self.p,self.a)
+                    self.key = mul[0] % self.n
+            else:
+                # use key provided as function parameter
+                self.key = key
+            
+            # calculate modular inverse of key
+            inv_key = pow(new_key,-1,self.n)
+            
+            # calculate y coordinate of signature
+            y = inv_key*(m_hash + pri_key * self.key) % self.n
+                        
+            # if input key returns y = 0, exit with custom error message to
+            # avoid endless loop
+            if key != None and y == 0:
+                raise Exception("inputted key returns y = 0")
+            
+            
+            self.signature = (self.key,y)
+        return self.signature
+    
+    # let B verify A's signature
+    def verify_signature(self, signature: tuple, m_hash: int, a_pub_key: tuple):
+        # verify that the signature point is generated correctly
+        for i in range(2):
+            if signature[i] == 0 or signature[i] > self.n:
+                raise Exception("signature is zero or bigger than n")
+        
+        inv_y = pow(signature[1],-1,self.n)
+        u1 = (m_hash*inv_y) % self.n
+        u2 = (signature[0]*inv_y) % self.n
+        g_u1 = curves.montgomery_ladder(self.G,u1,self.p,self.a)
+        a_pub_key_u2 = curves.montgomery_ladder(a_pub_key,u2,self.p,self.a)
+        pointX = curves.point_add(g_u1[0],g_u1[1],
+                                  a_pub_key_u2[0],a_pub_key_u2[1],
+                                  self.p,self.a)
+        
+        if pointX == (0,1):
+            print("generated verification signature is point at infinity")
+            return float("inf")
+        
+        if pointX[0] == signature[0]:
+            return True
+        return False
 
-# calculate shared secret
-# they are equal to each other, shared secret equals x coordinate
-a_shared_sec = weierstrass.multiply(bob.pub_k,alice.pri_k)[0]
-b_shared_sec = weierstrass.multiply(alice.pub_k,bob.pri_k)[0]
+class Ecies:
+    pass
 
-if a_shared_sec == b_shared_sec:
-    print("same secret key")
-
-ecdsa = Ecdsa(curve)
-
-# generate Alice's signature with ECDSA
-signature = ecdsa.gen_signature("abc", alice.pri_k)
-
-# let Bob verify Alice's signature
-verify_sign = ecdsa.verify_signature(signature, ecdsa.m_hash,
-                                     alice.pub_k)
-# print("signature gen: ", signature)
-print("signature sign:", verify_sign)
+# Elliptic Cryptography Diffie Hellman - Elliptic Cryptography Digital 
+# Signature Algorithm - 256-bit Advanced Encryption Standard - 
+# 512-bit Secure Hashing Algorithm
+class ecdhe_ecdsa_aes256_sha512:
+    pass
