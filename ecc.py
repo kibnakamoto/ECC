ee98c8f24b97c8d2b410d2506cb95b642d8fd745
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-07-30 19:07:59 -0400

    Create ecc.py

--- ecc.py
+++ ecc.py
@@ -1,36 +1,73 @@
 import curves
 from sha512 import *
+from hashlib import sha1,sha256,sha384 # optional
+from aes import *
 
 # same p,a,b parameters have to be used by both parties for 
 # creating a shared key, the shared key will be used for symmetric encryption and elliptic cryptography digital signatures
 def gen_shared_key(pri_k,pub_k, p, a):
     return curves.montgomery_ladder(pub_k, pri_k, p, a)[0]
 
+# field element to integer, used for non-prime fields
+# m = None is when prime field, not tested. Test with sect571k1
+def field_e_to_int(a, _2m, m=None):
+    if m == None:
+        return a
+    x = 0
+    bits = bin(a)[2:].zfill(m)
+    for i in range(m):
+        x = (x + int(bits[i])*2**i) % _2m
+    return x
+
 # Hash-based Message Authentication Code
-class Hmac:
-    pass
+def hmac(key: bytearray, msg: str,block_s: int = 64, machashf=sha256):
+    if len(key) == block_s:
+        new_key = bytearray(key)
+    elif len(key) > block_s:
+        new_key = machashf(key).digest()
+
+        # pad
+        for i in range(block_s-len(new_key)):
+            new_key += b'\x00'
+    else:
+        # pad
+        new_key = key
+        for i in range(len(key),block_s):
+            new_key += b'\x00'
+    
+    # calculate opad and ipad
+    ipadkey,opadkey = bytearray(),bytearray()
+    for i in range(block_s):
+        ipadkey.append(new_key[i] ^ 0x36)
+        opadkey.append(new_key[i] ^ 0x5c)
+    return machashf(opadkey + machashf((ipadkey + 
+                     msg.encode())).digest()).hexdigest()
 
 # Cipher-based Message Authentication Code
 class Cmac:
-    pass
+    def __init__(cipher=Aes256):
+        pass
 
-# sources (For finding resources, not representation)
-# ANSI X9.62
-# https://www.cs.miami.edu/home/burt/learning/Csc609.142/ecdsa-cert.pdf
-#  only for odd prime field size p. For field size q = 2^m, use integer conversion specified in ANSI X9.62
+#  only for odd prime field size p. For field size q = 2^m, use integer 
+#  conversion specified in ANSI X9.62
 class Ecdsa:
-    def __init__(self, p, G, n, a):
-        self.p = p
-        self.G = G
-        self.n = n
-        self.a = a
+    # def __init__(self, p, G, n, a):
+    def __init__(self, curve=curves.Secp521r1):
+        if hasattr(curve,"p"):
+            self.p = curve.p
+        else:
+            self.m = curve.m
+            self.size = curve.size
+        self.G = curve.G
+        self.n = curve.n
+        self.a = curve.a
     
     # generate A's signature
-    def gen_signature(self, message: str, pri_key: int, key: int = None):
+    def gen_signature(self, message: str, pri_key: int, key: int = None,hashf=Sha512):
         if key == 0:
             raise Exception("key x is zero")
         
-        m_hash = int(str(Sha512(message)),16)
+        m_hash = int(str(hashf(message).hexdigest()),16)
         self.m_hash = m_hash
         y = 0
 
@@ -83,7 +120,7 @@ class Ecdsa:
         pointX = curves.point_add(g_u1[0],g_u1[1],
                                   a_pub_key_u2[0],a_pub_key_u2[1],
                                   self.p,self.a)
-        print("signature sign: ",pointX)
+        
         if pointX == (0,1):
             print("generated verification signature is point at infinity")
             return float("inf")
